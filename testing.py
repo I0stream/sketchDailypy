2d9fcc7a4f306d2cd1d36d3393573b22c71d6199
Daniel <mr_schliesing@hotmail.com>
2022-02-28 10:37:10 -0900

    idk something last night. got next/prev working. button's functions were running on startup, solved by wrapping the functions with anonymous lamba functions

--- testing.py
+++ testing.py
@@ -8,30 +8,37 @@ Created on Sat Feb 26 10:43:53 2022
 
 import tkinter as tk
 from tkinter import filedialog
-
 from PIL import Image, ImageTk, ImageOps
-
 import json
-
 import glob
+from pathlib import Path
 
 
 current_image_index = 0
-def overflow_protect(nexPrev):
-    if current_image_index == 0 and nexPrev == -1:
-        current_image_index = len(files_grabbed)
-    elif current_image_index == len(files_grabbed) and nexPrev == 1:
-        current_image_index = 0
-    elif nexPrev == -1:
-        current_image_index = current_image_index -1
-    else:
-        current_image_index = current_image_index + 1
-    return current_image_index
+files_grabbed = []
 
 window = tk.Tk()
-window.geometry("750x500")
+window.geometry("1000x750")
 
 
+#store path
+def store_directory(path):
+    if path != "":
+        directory_path = {"path": path}
+        with open('data.json', 'w') as json_file:
+            json.dump(directory_path, json_file)
+
+def get_files(path, extensions):
+    for ext in extensions:
+        files_grabbed.extend(Path(path).glob(ext))
+    return files_grabbed
+
+def select_folder():
+    path = filedialog.askdirectory()
+    print(path)
+    store_directory(path)
+     # the tuple of file types
+    files_grabbed = get_files(path,('*.jpg', '*.png', '*.jpeg'))
 
 
 ###json
@@ -40,37 +47,17 @@ window.geometry("750x500")
 ##load, if empty do x
 with open('data.json', 'r') as f:
     data = json.load(f)
-    print(data)
     path = data['path']
 
 if path == "":
-    print("json path empty")
+    print("json path empty ", path)
 else:
-    print("we have a path in json")
-
-
-
-
-#also make an array with all the files in directory
-
-files_grabbed = []
-def select_folder():
-    path= filedialog.askdirectory()
-
-     # the tuple of file types
-    get_files(path)    
-    
-
-def get_files(path):
-    types = ('{path}/*.jpg', '{path}/*.png', '{path}/*.webp', '{path}/*.jpeg')
-    for files in types:
-        files_grabbed.extend(glob.glob(files))
-    print(files_grabbed)
+    files_grabbed = get_files(path,('*.jpg', '*.png', '*.jpeg'))
 
 
 ###image display
 
-image = Image.open("Answered-Prayers-Modern-Horizons.webp")
+image = Image.open(files_grabbed[current_image_index])
 image2 = ImageOps.contain(image, (512,512))
 test = ImageTk.PhotoImage(image2)
 
@@ -86,29 +73,58 @@ remaining = tk.Label(text="2/20", width="5",height="1")
 remaining.pack()
 
 
+###Play/pause next and previous
+
 def update_btn_text():
     if(play_pause["text"]=="play"):
         play_pause.configure(text="pause")
     else:
         play_pause.configure(text="play")
-play_pause = tk.Button(window, text="play", command=update_btn_text)
+
+play_pause = tk.Button(window, text="play", command=lambda: update_btn_text)
 play_pause.pack()
 
 
+
 def update_image(index):
-    newImage = Image.open(files_grabbed[overflow_protect(index)])
+    global current_image_index
+    if index:
+        current_image_index += 1
+    else:
+        current_image_index -= 1
+
+    if current_image_index <= 0 and not index:
+        current_image_index = len(files_grabbed)-1
 
 
-prev = tk.Button(text="prev", width="5",height="1", command=update_image(-1))
+    newImage = ImageTk.PhotoImage(Image.open(files_grabbed[index]))
+    imageLabel.configure(image=newImage2)
+    imageLabel.image = newImage
+
+
+
+
+prev = tk.Button(text="prev", width="5",height="1", command=lambda: update_image(False))
 prev.pack()
 
-next = tk.Button(text="next", width="5",height="1", command=update_image(1))
+next = tk.Button(text="next", width="5",height="1", command=lambda: update_image(True))
 next.pack()
 
 
 
-browserButton = tk.Button(text="browse", width="5",height="1", command= select_folder)
+#get directory, and then get files in directory
+
+
+
+
+
+
+
+browserButton = tk.Button(text="browse", width="5",height="1", command=lambda: select_folder)
 browserButton.pack()
+
+
+
 
 
 #####
